907dcbb57c62f2e04932cb9961b4e2bb5aaf7a0a
polynomeer <polynomeer@gmail.com>
2021-02-03 11:53:01 +0900

    1st Solved prgms_12906

--- ByJava/src/prgms/prgms_12906.java
+++ ByJava/src/prgms/prgms_12906.java
@@ -3,13 +3,21 @@ package prgms;
 import java.util.*;
 
 class Solution_12906 {
-    public Integer[] solution(int []arr) {
-        Set<Integer> set = new LinkedHashSet<>();
-        for (int num : arr){
-            set.add(num);
+    public List<Integer> solution(int[] arr) {
+        if (arr.length == 0) return null;
+        List<Integer> answer = new ArrayList<>();
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (arr[i] != arr[i + 1]) {
+                answer.add(arr[i]);
+            }
         }
-        Integer[] answer = new Integer[set.size()];
-        set.toArray(answer);
+        if (answer.size() == 0){
+            answer.add(arr[0]);
+            return answer;
+        }
+        if (answer.get(answer.size() - 1) != arr[arr.length - 1]) {
+            answer.add(arr[arr.length - 1]);
+        }
         return answer;
     }
 }
@@ -17,6 +25,9 @@ class Solution_12906 {
 public class prgms_12906 {
     public static void main(String[] args) {
         Solution_12906 sol = new Solution_12906();
-        System.out.println();
+        System.out.println(sol.solution(new int[]{1, 1, 3, 3, 0, 1, 1}));
+        System.out.println(sol.solution(new int[]{4, 4, 4, 3, 3}));
+        System.out.println(sol.solution(new int[]{}));
+        System.out.println(sol.solution(new int[]{1, 1, 1}));
     }
 }
