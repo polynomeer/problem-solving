dd91bbffac3eb43d7dfc1727a8ce2e2bc4c519ba
polynomeer <polynomeer@gmail.com>
2021-02-04 22:16:12 +0900

    2nd Solved prgms_42576

--- ByJava/src/prgms/prgms_42576.java
+++ ByJava/src/prgms/prgms_42576.java
@@ -1,7 +1,6 @@
 package prgms;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class prgms_42576 {
     public static void main(String[] args) {
@@ -9,9 +8,51 @@ public class prgms_42576 {
         System.out.println(sol.solution(new String[]{"leo", "kiki", "eden"}, new String[]{"eden", "kiki"}));
         System.out.println(sol.solution(new String[]{"marina", "josipa", "nikola", "vinko", "filipa"}, new String[]{"josipa", "filipa", "marina", "nikola"}));
         System.out.println(sol.solution(new String[]{"mislav", "stanko", "mislav", "ana"}, new String[]{"stanko", "ana", "mislav"}));
+        System.out.println(sol.solution(new String[]{"august", "august", "august"}, new String[]{"august", "august"}));
     }
 }
 
+class Solution_42576 {
+    public String solution(String[] participant, String[] completion) {
+        Map<String, Integer> map = new HashMap<>();
+
+        for (String p : participant) {
+            if (!map.containsKey(p)) {
+                map.put(p, 1);
+            } else {
+                map.put(p, map.get(p) + 1);
+            }
+        }
+        for (String c : completion) {
+            if (map.containsKey(c)) {
+                if (map.get(c) <= 1) {
+                    map.remove(c);
+                } else {
+                    map.put(c, map.get(c) - 1);
+                }
+            }
+        }
+        for (String key : map.keySet()) {
+            return key;
+        }
+        return "";
+    }
+}
+/*class Solution_42576 {
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        Set<String> set = new HashSet<>(Arrays.asList(participant));
+        for (String c : completion){
+            set.remove(c);
+        }
+        for (String s : set){
+            return s;
+        }
+        return "";
+    }
+}*/
+
+/*
 class Solution_42576 {
     public String solution(String[] participant, String[] completion) {
         String answer = "";
@@ -40,3 +81,4 @@ class Solution_42576 {
         return answer;
     }
 }
+*/
