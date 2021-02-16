1d210773cc2c6b0bc4308efe2e2df0c9c1b9d0a9
Polynomeer <polynomeer@gmail.com>
2021-02-16 14:45:58 +0900

    1st Solved prgms_42746

--- ByJava/src/prgms/prgms_42746.java
+++ ByJava/src/prgms/prgms_42746.java
@@ -4,18 +4,24 @@ import java.util.*;
 
 class Solution_42746 {
     public String solution(int[] numbers) {
-        TreeSet<String> set = new TreeSet<>(Comparator.reverseOrder());
-        for (int n : numbers){
-            set.add(Integer.toString(n));
+        String[] str = new String[numbers.length];
+
+        for (int i = 0; i < numbers.length; i++) {
+            str[i] = String.valueOf(numbers[i]);
         }
-        return combine(set);
+
+        Arrays.sort(str, (s1, s2) -> ((s2 + s1).compareTo(s1 + s2)));
+
+        return str[0].equals("0") ? "0" : combine(str);
     }
 
-    private String combine(TreeSet<String> set){
+    private String combine(String[] strings) {
         StringBuilder sb = new StringBuilder();
-        for (String s : set){
+
+        for (String s : strings) {
             sb.append(s);
         }
+
         return sb.toString();
     }
 }
@@ -25,5 +31,10 @@ public class prgms_42746 {
         Solution_42746 sol = new Solution_42746();
         System.out.println(sol.solution(new int[]{6, 10, 2}));
         System.out.println(sol.solution(new int[]{3, 30, 34, 5, 9}));
+        System.out.println(sol.solution(new int[]{0, 0, 0, 0}));
+        System.out.println(sol.solution(new int[]{21, 212}));
+        System.out.println(sol.solution(new int[]{1, 1, 1, 1}));
+        System.out.println(sol.solution(new int[]{12, 12}));
+        System.out.println(sol.solution(new int[]{123, 123}));
     }
 }
