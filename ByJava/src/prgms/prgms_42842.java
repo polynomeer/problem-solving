556207a00b8bfbbc2432206a93d444b8e45b51da
Polynomeer <polynomeer@gmail.com>
2021-02-16 12:00:52 +0900

    1st Solved prgms_43165

---
+++ ByJava/src/prgms/prgms_42842.java
@@ -0,0 +1,24 @@
+package prgms;
+
+import java.util.Arrays;
+
+class Solution_42842 {
+    public int[] solution(int brown, int yellow) {
+        int[] answer = {};
+        for (int i = 1; i <= yellow; i++) {
+            if (yellow / i == 0){
+
+            }
+        }
+        return answer;
+    }
+}
+
+public class prgms_42842 {
+    public static void main(String[] args) {
+        Solution_42842 sol = new Solution_42842();
+        System.out.println(Arrays.toString(sol.solution(10, 2)));
+        System.out.println(Arrays.toString(sol.solution(8, 1)));
+        System.out.println(Arrays.toString(sol.solution(24, 24)));
+    }
+}
