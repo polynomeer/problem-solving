dd5faf0211d44a15b40447b8d8af09210a28d48a
Polynomeer <polynomeer@gmail.com>
2021-03-17 20:24:30 +0900

    1st Solved Codility - PermMissingElem

--- ByJava/src/codility/PermMissingElem.java
+++ ByJava/src/codility/PermMissingElem.java
@@ -5,12 +5,14 @@ import java.util.Arrays;
 class PermMissingElemSolution {
     public int solution(int[] A) {
         Arrays.sort(A);
+        if (A.length == 0) return 1;
+        if (A[0] != 1 ) return 1;
         for (int i = 0; i < A.length - 1; i++) {
             if (A[i] + 1 != A[i+1]) {
                 return A[i] + 1;
             }
         }
-        return 0;
+        return A.length + 1;
     }
 }
 
@@ -18,5 +20,9 @@ public class PermMissingElem {
     public static void main(String[] args) {
         PermMissingElemSolution sol = new PermMissingElemSolution();
         System.out.println(sol.solution(new int[]{2, 3, 1, 5}));
+        System.out.println(sol.solution(new int[]{2, 3, 4, 5}));
+        System.out.println(sol.solution(new int[]{2, 3, 4, 1}));
+        System.out.println(sol.solution(new int[]{3, 1}));
+        System.out.println(sol.solution(new int[]{1}));
     }
 }
