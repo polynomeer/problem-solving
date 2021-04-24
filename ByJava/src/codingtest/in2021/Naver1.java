ec4b7c0b93ffd49b8f91a8f1d4546db738080324
Polynomeer <polynomeer@gmail.com>
2021-04-24 23:39:15 +0900

    Initialize 2021 Naver Recruitment - 1st problem

---
+++ ByJava/src/codingtest/in2021/Naver1.java
@@ -0,0 +1,23 @@
+package codingtest.in2021;
+
+class Naver1Solution {
+    String solution(int[][] scores) {
+        String answer = "";
+        return answer;
+    }
+}
+
+public class Naver1 {
+    public static void main(String[] args) {
+        Naver1Solution sol = new Naver1Solution();
+        int[][] scores = new int[][]
+                {
+                        {100, 75, 84, 45, 36},
+                        {55, 45, 65, 48, 56},
+                        {75, 66, 74, 95, 83},
+                        {83, 90, 92, 86, 99},
+                        {42, 36, 75, 74, 98}
+                };
+        System.out.println(sol.solution(scores));
+    }
+}
