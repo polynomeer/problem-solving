ef1cacc6120a14d952e2e91c9f395a9c97dd5765
Polynomeer <polynomeer@gmail.com>
2021-10-31 00:00:10 +0900

    Add WinterNaverFinancial2

---
+++ ByJava/src/codingtest/in2021/WinterNaverFinancial2.java
@@ -0,0 +1,28 @@
+package codingtest.in2021;
+
+import java.util.Arrays;
+
+class WinterNaverFinancial2Solution {
+    public int[] solution(int n, int jump) {
+        int[] answer = {};
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+
+
+            }
+
+        }
+
+        return answer;
+    }
+}
+
+public class WinterNaverFinancial2 {
+    public static void main(String[] args) {
+        WinterNaverFinancial2Solution sol = new WinterNaverFinancial2Solution();
+        System.out.println(Arrays.toString(sol.solution(5, 3)));
+        System.out.println(Arrays.toString(sol.solution(4, 1)));
+        System.out.println(Arrays.toString(sol.solution(3, 10)));
+    }
+}
