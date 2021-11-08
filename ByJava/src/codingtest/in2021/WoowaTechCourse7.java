d7da7cfa2c9ad04a0810bf8ca33f2e2048ad695b
Polynomeer <polynomeer@gmail.com>
2021-11-08 17:08:11 +0900

    Add WoowaTechCourse7

---
+++ ByJava/src/codingtest/in2021/WoowaTechCourse7.java
@@ -0,0 +1,19 @@
+package codingtest.in2021;
+
+import java.util.Arrays;
+
+class WoowaTechCourse7Solution {
+    public String[] solution(String[] grid, boolean clockwise) {
+        String[] answer = {};
+        return answer;
+    }
+}
+
+public class WoowaTechCourse7 {
+    public static void main(String[] args) {
+        WoowaTechCourse7Solution sol = new WoowaTechCourse7Solution();
+        System.out.println(Arrays.toString(sol.solution(new String[]{"1", "234", "56789"}, true)));
+        System.out.println(Arrays.toString(sol.solution(new String[]{"A", "MAN", "DRINK", "WATER11"}, false)));
+
+    }
+}
