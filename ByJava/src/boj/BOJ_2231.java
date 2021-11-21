f7a38ce799a7931e0e0bfd5e925ec3f4df08798c
polynomeer <polynomeer@gmail.com>
2021-11-21 22:20:24 +0900

    Add Baekjoon Online Judge - 2231 solution

---
+++ ByJava/src/boj/BOJ_2231.java
@@ -0,0 +1,35 @@
+package boj;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class BOJ_2231 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int answer = 0;
+        List<Integer> decompositions = new ArrayList<>();
+
+        for (int i = n; i >= 0; i--) {
+            if (getDecomposition(i) == n) {
+                decompositions.add(i);
+            }
+        }
+
+        if (!decompositions.isEmpty()) {
+            answer = decompositions.get(decompositions.size() - 1);
+        }
+
+        System.out.println(answer);
+    }
+
+    private static int getDecomposition(int m) {
+        int sum = m;
+        while (m > 0) {
+            sum += m % 10;
+            m /= 10;
+        }
+        return sum;
+    }
+}
