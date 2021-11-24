0d38a1c31054ac3709f789b975893941c1485b2c
polynomeer <polynomeer@gmail.com>
2021-11-24 23:50:18 +0900

    Add Channel Corporation 1 : failed

---
+++ ByJava/src/codingtest/in2021/ChannelCorporation1.java
@@ -0,0 +1,20 @@
+package codingtest.in2021;
+
+class ChannelCorporationSolution1 {
+    public String solution(String exp1, String exp2) {
+        String answer = "";
+
+        String[] tokens1;
+        return answer;
+    }
+}
+
+public class ChannelCorporation1 {
+
+    public static void main(String[] args) {
+        ChannelCorporationSolution1 sol = new ChannelCorporationSolution1();
+
+        System.out.println(sol.solution("2x^5 + 3x^3 + 2x^1", "6x^6 + 4x^4 + 3x^3 + 2x^2"));
+
+    }
+}
