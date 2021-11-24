ccfb6037013ecd8365edbabcba7451304ba4b845
polynomeer <polynomeer@gmail.com>
2021-11-24 23:49:23 +0900

    Update Iportforlio - 1 failed

--- ByJava/src/codingtest/in2021/Iportforlio1.java
+++ ByJava/src/codingtest/in2021/Iportforlio1.java
@@ -1,27 +1,8 @@
 package codingtest.in2021;
 
-import java.util.Arrays;
-
 class IportforlioSolution1 {
     public int solution(int n) {
-        boolean[] doors = new boolean[n + 1];
-        int answer = 0;
-
-        Arrays.fill(doors, true);
-
-        for (int i = 1; i <= n; i++) {
-            if (i % 2 == 0) {
-                doors[i] = i % 3 == 0;
-            } else {
-                if (i % 3 == 0) doors[i] = !doors[i];
-            }
-        }
-
-        for (int i = 1; i < doors.length; i++) {
-            if (doors[i]) answer++;
-        }
-
-        return answer;
+        return n - n / 2 - n / 3 + n / 6 * 2;
     }
 }
 
@@ -30,5 +11,7 @@ public class Iportforlio1 {
         IportforlioSolution1 sol = new IportforlioSolution1();
         System.out.println(sol.solution(2));
         System.out.println(sol.solution(5));
+        System.out.println(sol.solution(9));
+        System.out.println(sol.solution(10));
     }
 }
