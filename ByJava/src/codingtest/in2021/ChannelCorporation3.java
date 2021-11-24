45a42fa54e00e91e5d4481f0fdfd0db46e99ab87
polynomeer <polynomeer@gmail.com>
2021-11-24 23:50:47 +0900

    Add Channel Corporation 2 : failed

---
+++ ByJava/src/codingtest/in2021/ChannelCorporation3.java
@@ -0,0 +1,21 @@
+package codingtest.in2021;
+
+class ChannelCorporationSolution3 {
+    public int solution(int[][] R) {
+        int answer = 0;
+
+
+        return answer;
+    }
+}
+
+public class ChannelCorporation3 {
+    public static void main(String[] args) {
+        ChannelCorporationSolution3 sol = new ChannelCorporationSolution3();
+        System.out.println(sol.solution(
+                new int[][]{{3, 9, 2, 7},
+                        {10, 6, 8, 4},
+                        {1, 4, 9, 10},
+                        {5, 7, 8, 4}}));
+    }
+}
