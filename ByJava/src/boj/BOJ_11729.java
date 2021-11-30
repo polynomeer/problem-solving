fcd4a9a542c4bd687bea8008c291585c6a23a1d9
polynomeer <polynomeer@gmail.com>
2021-11-30 23:46:03 +0900

    Add Baekjoon Online Judge 11729 : solved

--- ByJava/src/boj/BOJ_11729.java
+++ ByJava/src/boj/BOJ_11729.java
@@ -3,19 +3,23 @@ package boj;
 import java.util.Scanner;
 
 public class BOJ_11729 {
+    private static final StringBuilder sb = new StringBuilder();
+
     private static void hanoi(int n, int start, int to, int via) {
         if (n == 1) {
-            System.out.println(start + " " + to);
+            sb.append(start).append(" ").append(to).append("\n");
             return;
         }
         hanoi(n - 1, start, via, to);
-        System.out.println(start + " " + to);
+        sb.append(start).append(" ").append(to).append("\n");
         hanoi(n - 1, via, to, start);
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        sb.append((int) Math.pow(2, n) - 1).append('\n');
         hanoi(n, 1, 3, 2);
+        System.out.print(sb);
     }
 }
