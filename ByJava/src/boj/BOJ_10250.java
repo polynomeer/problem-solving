a492c4f9653209f48be7e7d3cd3d94d7c1a5e478
polynomeer <polynomeer@gmail.com>
2021-11-30 20:44:40 +0900

    Add Baekjoon Online Judge 10250 : solved

--- ByJava/src/boj/BOJ_10250.java
+++ ByJava/src/boj/BOJ_10250.java
@@ -3,21 +3,30 @@ package boj;
 import java.util.Scanner;
 
 public class BOJ_10250 {
-    private static void solve(int h, int w, int n) {
+    private static String solve(int h, int w, int n) {
         int floor = n % h;
         int number = n / h + 1;
-        System.out.printf("%d%02d\n", floor, number);
+        if (n % h == 0) {
+            floor = h;
+            number = n / h;
+        }
+        return String.format("%d%02d", floor, number);
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int T = sc.nextInt();
+        String[] answers = new String[T];
 
         for (int testcase = 0; testcase < T; testcase++) {
             int H = sc.nextInt();
             int W = sc.nextInt();
             int N = sc.nextInt();
-            solve(H, W, N);
+            answers[testcase] = solve(H, W, N);
+        }
+
+        for (String answer : answers) {
+            System.out.println(answer);
         }
     }
 }
