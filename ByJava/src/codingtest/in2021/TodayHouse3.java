98468b358c9d10fe431d491b3785b5e9a56012bc
polynomeer <polynomeer@gmail.com>
2021-12-06 23:10:11 +0900

    Add Today's House coding test 3 : failed

---
+++ ByJava/src/codingtest/in2021/TodayHouse3.java
@@ -0,0 +1,25 @@
+package codingtest.in2021;
+
+import java.util.Arrays;
+
+class TodayHouseSolution3 {
+    public int[] solution(int a, int b) {
+        int[] answer = new int[2];
+        int[] player = new int[2];
+        int n = a + b;
+
+        for (int i = 0; i < n; i++) {
+
+        }
+
+        return answer;
+    }
+}
+
+public class TodayHouse3 {
+    public static void main(String[] args) {
+        TodayHouseSolution3 sol = new TodayHouseSolution3();
+        System.out.println(Arrays.toString(sol.solution(0, 100)));
+        System.out.println(Arrays.toString(sol.solution(1, 2)));
+    }
+}
