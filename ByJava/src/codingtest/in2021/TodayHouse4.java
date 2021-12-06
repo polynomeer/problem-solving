94cdac2f230ddae43bed47c563434f7da01c0600
polynomeer <polynomeer@gmail.com>
2021-12-06 23:10:24 +0900

    Add Today's House coding test 4 : failed

---
+++ ByJava/src/codingtest/in2021/TodayHouse4.java
@@ -0,0 +1,16 @@
+package codingtest.in2021;
+
+class TodayHouseSolution4 {
+    public int solution(String[] isAvailable, int N, int M, int H, int W) {
+        int answer = 0;
+        return answer;
+    }
+}
+
+public class TodayHouse4 {
+    public static void main(String[] args) {
+        TodayHouseSolution4 sol = new TodayHouseSolution4();
+        System.out.println(sol.solution(new String[]{"1111", "1011", "1011", "1111"}, 4, 4, 2, 3));
+        System.out.println(sol.solution(new String[]{"111110", "110110", "110110", "111110", "111110"}, 5, 6, 3, 4));
+    }
+}
