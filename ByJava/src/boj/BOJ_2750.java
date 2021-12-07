0ed5043d011fbc6b992c0e9895ee998b043b8adc
polynomeer <polynomeer@gmail.com>
2021-12-07 22:11:48 +0900

    Add Baekjoon Online Judge 2750 : solved

---
+++ ByJava/src/boj/BOJ_2750.java
@@ -0,0 +1,19 @@
+package boj;
+
+import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class BOJ_2750 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        Set<Integer> set = new TreeSet<>();
+
+        for (int i = 0; i < n; i++) {
+            set.add(sc.nextInt());
+        }
+
+        set.forEach(System.out::println);
+    }
+}
