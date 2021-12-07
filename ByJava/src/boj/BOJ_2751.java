21e9f5b44b55b74f3ea8ab95df59ac7f70000bc8
polynomeer <polynomeer@gmail.com>
2021-12-07 22:22:00 +0900

    Add Baekjoon Online Judge 2751 : failed

--- ByJava/src/boj/BOJ_2751.java
+++ ByJava/src/boj/BOJ_2751.java
@@ -1,22 +1,19 @@
 package boj;
 
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
 
 public class BOJ_2751 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int[] arr = new int[n];
+        List<Integer> list = new ArrayList<>();
 
         for (int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
+            list.add(sc.nextInt());
         }
 
-        Arrays.sort(arr);
+        Collections.sort(list);
 
-        for (int a : arr) {
-            System.out.println(a);
-        }
+        list.forEach(System.out::println);
     }
 }
