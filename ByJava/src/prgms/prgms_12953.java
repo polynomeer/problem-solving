e0d6f1107d52702854a7c80e146a9f59807ed261
polynomeer <polynomeer@gmail.com>
2021-12-11 20:46:56 +0900

    Add Programmers 12953 : solved

---
+++ ByJava/src/prgms/prgms_12953.java
@@ -0,0 +1,29 @@
+package prgms;
+
+import java.util.Arrays;
+
+class Solution_12953 {
+    public int solution(int[] arr) {
+        Arrays.sort(arr);
+        int i = 1;
+        for (; i < Math.pow(100, 15); i++) {
+            if (isDivided(arr, i)) break;
+        }
+        return i;
+    }
+
+    private boolean isDivided(int[] arr, int i) {
+        for (int k : arr) {
+            if (i % k != 0) return false;
+        }
+        return true;
+    }
+}
+
+public class prgms_12953 {
+    public static void main(String[] args) {
+        Solution_12953 sol = new Solution_12953();
+        System.out.println(sol.solution(new int[]{2, 6, 8, 14}) == 168);
+        System.out.println(sol.solution(new int[]{1, 2, 3}) == 6);
+    }
+}
