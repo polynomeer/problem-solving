7b8e79d7cb3e2bf5ae8fc177405463cc690f5964
polynomeer <polynomeer@gmail.com>
2025-08-29 23:58:06 +0900

    perf: add advanced solution for container with most water

--- java-solution/src/leetcode/ContainerWithMostWater.java
+++ java-solution/src/leetcode/ContainerWithMostWater.java
@@ -2,13 +2,31 @@ package leetcode;
 
 public class ContainerWithMostWater {
     static class Solution {
-        public int maxArea(int[] height) {
+        public int maxAreaV1(int[] height) {
             int max = -987654321;
             for (int i = 0; i < height.length; i++) {
                 for (int j = i + 1; j < height.length; j++) {
                     max = Math.max(Math.min(height[i], height[j]) * (j - i), max);
                 }
             }
+            return max;
+        }
+
+        public int maxArea(int[] height) {
+            int max = 0;
+            int left = 0, right = height.length - 1;
+
+            while (left < right) {
+                int currentArea = Math.min(height[left], height[right]) * (right - left);
+                max = Math.max(max, currentArea);
+
+                if (height[left] < height[right]) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+
             return max;
         }
 
